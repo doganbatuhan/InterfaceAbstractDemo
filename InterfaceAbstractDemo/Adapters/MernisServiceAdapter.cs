8bd9cf4a39f12c34a1aff4740c42aaee5c9fbe00
doganbatuhan <batuuhan.dogan@gmail.com>
2021-01-24 19:14:45 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(),
+                customer.LastName.ToUpper(),customer.DateOfBirth.Year);
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1996, 1, 1),
+                FirstName = "Batuhan",
+                LastName = "Doğan",
+                NationalityId = ""
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
